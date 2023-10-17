4df8f3b7d4a41b621cd5e60e61d7f3000c66b9a1
jerrita <je5r1ta@icloud.com>
2023-10-17 20:53:12 +0800

    sync

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         home-manager = {
-            url = "github:nix-community/home-manager/release-23.05";
+            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
--- hosts/aris/default.nix
+++ hosts/aris/default.nix
@@ -7,6 +7,7 @@
       ./hardware-configuration.nix
     ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
