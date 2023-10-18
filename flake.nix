2dd8359b14f19ffc4c794ca2b7fba2e83154a73d
jerrita <je5r1ta@icloud.com>
2023-10-18 13:44:13 +0800

    change mirror

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixConfig = {
         experimental-features = [ "nix-command" "flakes" ];
         substituters = [
-            "https://mirrors.ustc.edu.cn/nix-channels/store"
+            "https://repo.mirrors.cqupt.edu.cn/repository/nix-channels/store"
         ];
 
         # nix community's cache server
--- scratch.nix
+++ scratch.nix
@@ -30,7 +30,7 @@
 
   services.openssh.enable = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  nix.settings.substituters = [ "https://mirrors.ustc.edu.cn/nix-channels/store" ];
+  nix.settings.substituters = [ "https://repo.mirrors.cqupt.edu.cn/repository/nix-channels/store" ];
   system.stateVersion = "23.05";
 }
 
