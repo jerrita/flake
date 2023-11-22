2e41cd6f82306cdf2e50672fa90a282cce92fa04
jerrita <je5r1ta@icloud.com>
2023-11-22 20:13:40 +0800

    sync

--- flake.nix
+++ flake.nix
@@ -17,9 +17,9 @@
     };
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
         home-manager = {
-            url = "github:nix-community/home-manager";
+            url = "github:nix-community/home-manager/release-23.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
